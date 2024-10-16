9a1e1ab6a1e3b50c11a105a396a28f559ba574f1
Kshubh0 <161875795+Kshubh0@users.noreply.github.com>
2024-10-16 18:42:46 +0000

    datatypes and variables

---
+++ basics/01_variable.js
@@ -0,0 +1,11 @@
+const id= "12345";
+let namee ="shubham"
+var email = "abc@com"
+
+email= "new@com"
+namee ="Rajesh"
+console.log(id)
+
+console.log(namee);
+
+console.table([id, namee,email]);
---
+++ basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use_strict";  //treat code in newer version of javascript
+
+
+let age = 18
+let isLoggedin = false
+let state;
+
+//number => 2 to power 53
+//string => " "
+//bigint
+//boolean => true/false
+//null => standalone value
+//undefined => not assigned any value
+//symbol => unique
+
+// object
+
+
